46cf6aedbc2ab5e79afea5aeb3192ea0568c9a23
aliocantinea <aliocantinea@gmail.com>
2022-05-26 18:36:33 +0000

    Thu, May 26, 2022, 6:36 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,24 +10,24 @@ int main(void)
     {
         do
         {
-        n = get_int("Height: ");
+            n = get_int("Height: ");
         }
-        while (n>8);
+        while (n > 8);
     }
-    while (n<1);
+    while (n < 1);
 
     //take n var and print # per line until it matches n
     //for creating user defined rows
-    for (int r=0; r<n; r++)
+    for (int r = 0; r < n; r++)
     {
         //placeholder d for dots to fill in rows
         //-1 for # as it has to equal at least 1 per row
-        for (int d=n-1; d>r; d--)
+        for (int d = n - 1; d > r; d--)
         {
             printf(" ");
         }
         //loop to create collumns, how many times repeats until \n
-        for (int c=0; c<=r; c++)
+        for (int c = 0; c <= r; c++)
         {
             printf("#");
         }
