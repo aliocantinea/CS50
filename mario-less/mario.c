a975cf52a35488b3431de9a064399c0da2b5e946
aliocantinea <aliocantinea@gmail.com>
2022-05-26 17:04:56 +0000

    Thu, May 26, 2022, 5:04 PM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //get int between 1-8
+    //  **only asking one time** how do I get it to be a valid number
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n>=8);
+
+    //take n var and print # per line until it matches n
+    //for creating user defined rows
+    for (int r=0; r<n; r++)
+    {
+        //loop to create collumns, how many times repeats until \n
+        for (int c=0; c<n; c++)
+        {
+            printf("#");
+        }
+        //placeholder to fill in rows
+        for (int ph=n; ph<n; ph--)
+        {
+            printf(".");
+        }
+        //returns after line
+        printf("\n");
+    }
+}
