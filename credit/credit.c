020d8c270db6a87c276ca91b48ac9ff718b24df5
aliocantinea <aliocantinea@gmail.com>
2022-05-29 20:47:48 +0000

    Sun, May 29, 2022, 8:47 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -19,32 +19,32 @@ int main(void)
     {
         //do when between 13-16 digits long
         long v = 0; //v = verifying number, needed outside of mod cal
-        printf("%li\n\n", v);
+        //printf("%li\n\n", v);
         for (long d = 1; d < n ;d = d * 10) //d = divisible
         {
             long cd; //cd = card digit
-            cd = n / d % 10; //reduces card length to get mod
-            //cd = cd % 10;
-            printf("%li\n", cd);
+            cd = n / d % 10; //reduces card length by factor of 10 and gets mod
+            //printf("%li\n", cd);
             v = v + cd; //adds last and every second to v
-            d = d * 10; //
-            cd = n / d % 10 *2;
-            //cd = cd % 10 * 2;
+            d = d * 10; //reduces card length by 10 for every sencond digit
+            cd = n / d % 10 *2; //doubles every sencond digit from last
             if (cd >= 10)
             {
-                cd = cd - 10 +1;
-
-                printf("1+%li\n", cd);
+                cd = cd - 10 +1; //removes second digit and adds one for it
+                //printf("1+%li\n", cd);
                 v = v + cd;
-
             }
             else
             {
-                printf("%li\n", cd);
+                //printf("%li\n", cd);
                 v = v + cd;
             }
         }
-        printf("\n%li\n\n", v);
+        //mod of v to see if it ends in 0
+        printf("\n%li\n", v);
+        v = v %10;
+        printf("mod %li\n", v);
+        //if ( )
 
     }
 }
