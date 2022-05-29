5b3b4e75cabbf08f87049b5e2ed74184770d55bb
aliocantinea <aliocantinea@gmail.com>
2022-05-29 03:29:44 +0000

    Sun, May 29, 2022, 3:29 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,14 +15,13 @@ int main(void)
     }
     else
     {
-        printf ("%li\n", n);
-    }
         //do when between 13-16
-    //check by first number
-    //printf ("%li\n", n);
+        //check by first number
+        printf ("%li\n", n);
                 //AE 3, MC 5, V 4
     //modulous equation % after / by digits comparaible to 10 so the remainder is 0-10
         //number /10 will get the second to last digit
         //if you /1000 you will get the 4th digit
     //if >10 /10 and % to get both numbers and add to total
+    }
 }
