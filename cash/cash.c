f5a635165b4656dcd0b31f9a736773f93d5232de
aliocantinea <aliocantinea@gmail.com>
2022-05-29 01:36:44 +0000

    Sun, May 29, 2022, 1:36 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_cents(void);
+int get_cents(c);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
@@ -10,7 +10,8 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents(int c);
+    int cents = get_cents(c);
+    printf ("%c\n")
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
