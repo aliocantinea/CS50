ccfc614072282ab117db20d853e8cfc9bbb01e3d
aliocantinea <aliocantinea@gmail.com>
2022-05-29 04:03:37 +0000

    Sun, May 29, 2022, 4:03 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -16,7 +16,7 @@ int main(void)
     else
     {
         //do when between 13-16 digits long
-        long cn = n % 10;
+        long cn = n % 1000;
         //check by first number
         if (cn == 4)
         {
