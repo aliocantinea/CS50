2bab453386c565b335cb91a6b86b7aaee1e39ebc
aliocantinea <aliocantinea@gmail.com>
2022-05-29 03:08:48 +0000

    Sun, May 29, 2022, 3:08 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,11 +12,12 @@ int main(void)
             n = get_long("Number: ");
         }
         //do when between 13-16
-        while (n < 3000000000000);
+        while (n > 5000000000000000);
+        printf ("INVALID");
     }
     while (n > );
     //check by first number
-    printf("%li\n", n);
+    printf ("%li\n", n);
                 //AE 3, MC 5, V 4
     //modulous equation % after / by digits comparaible to 10 so the remainder is 0-10
         //number /10 will get the second to last digit
