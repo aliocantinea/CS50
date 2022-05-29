37361fd52de3d3c64ff977779db0f1efb6e2f0bd
aliocantinea <aliocantinea@gmail.com>
2022-05-29 01:53:17 +0000

    Sun, May 29, 2022, 1:53 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,7 +10,7 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents(int c);
+    int cents = get_cents(%i, c);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
