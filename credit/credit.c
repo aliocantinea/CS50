761cc305c3ceb1d97146ae308ae6bdd030daa116
aliocantinea <aliocantinea@gmail.com>
2022-05-29 05:54:36 +0000

    Sun, May 29, 2022, 5:54 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -20,12 +20,13 @@ int main(void)
         for (long m = 10; m < n*10 ;)
         {
             cn = n % m;
-            if (cn < 10)
+            if (cn > 10)
             {
-                cn = cn / m;
+                cn = cn / 10;
                 printf("%li\n", cn);
             }
             else
+            
             printf("%li\n", cn);
             m = m * 10;
         }
