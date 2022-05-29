27d590d546b44e85b37c35d777ea062af4112458
aliocantinea <aliocantinea@gmail.com>
2022-05-29 05:35:10 +0000

    Sun, May 29, 2022, 5:35 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -17,10 +17,11 @@ int main(void)
     {
         //do when between 13-16 digits long
         long cn;
-        for (long m = 1; m <  * 10 ; m++)
+        for (long m = 1; m < n*10 ;)
         {
             cn = n % m;
-            printf("%li\n", cn); // * 10 / 1
+            printf("%li\n", cn);
+            m = m * 10; // * 10 / 1
         }
         //modulous equation % after / by digits comparaible to 10 so the remainder is 0-10
             //number /10 will get the second to last digit
