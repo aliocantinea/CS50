abf7aa97cb0b0aae492e49f1d0963444e1d3945f
aliocantinea <aliocantinea@gmail.com>
2022-05-30 00:41:39 +0000

    Mon, May 30, 2022, 12:41 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,41 +12,41 @@ int main(void)
     long n = get_long("Number: ");
     //do when between 13-16 digits long
     long v = 0; //v = verifying number, needed outside of mod cal
-                                //printf("    %li\n\n", v);
+    //printf("    %li\n\n", v);
     for (long d = 1; d < n ; d = d * 10) //d = divisible
     {
         long cd; //cd = card digit
         cd = n / d % 10; //reduces card length by factor of 10 and gets mod
-                                //printf("%li\n", cd);
+        //printf("%li\n", cd);
         v = v + cd; //adds last and every second to v
-                                //printf("    %li\n", v);
+        //printf("    %li\n", v);
         d = d * 10; //reduces card length by 10 for every sencond digit
         cd = n / d % 10 * 2; //doubles every sencond digit from last
         if (cd >= 10)
         {
             cd = cd - 10 + 1; //removes second digit and adds one for it
-                                //printf("(1+)%li\n", cd);
+            //printf("(1+)%li\n", cd);
             v = v + cd;
-                                //printf("    %li\n", v);
+            //printf("    %li\n", v);
         }
         else
         {
-                                //printf("%li\n", cd);
+            //printf("%li\n", cd);
             v = v + cd;
-                                //printf("    %li\n", v);
+            //printf("    %li\n", v);
         }
     }
     //mod of v to see if it ends in 0
-                                //printf("\n    %li\n", v);
+    //printf("\n    %li\n", v);
     v = v % 10;
-                                //printf("mod %li\n", v);
+    //printf("mod %li\n", v);
     if (v == 0)
     {
         n = n / 1000000000000;
-                                //printf("%li\n\n", n);
+        //printf("%li\n\n", n);
         if (n == 4)
         {
-                                //printf("%li\n", n);
+            //printf("%li\n", n);
             //tests for 13 vs
             vs();
         }
@@ -55,7 +55,7 @@ int main(void)
             n = n / 10;
             if (n == 34 || n == 37)
             {
-                                //printf("%li\n", n);
+                //printf("%li\n", n);
                 //tests for am either 34 or 37
                 am();
             }
@@ -64,10 +64,10 @@ int main(void)
                 n = n / 10;
                 if (n >= 51) //above min for mc
                 {
-                                //printf("%li\n", n);
+                    //printf("%li\n", n);
                     if (n <= 55) //if below max inv
                     {
-                                //printf("%li\n", n);
+                        //printf("%li\n", n);
                         mc();
                     }
                     else
@@ -80,7 +80,7 @@ int main(void)
                     n = n / 10;
                     if (n == 4)
                     {
-                                //printf("%li\n", n);
+                        //printf("%li\n", n);
                         //checks 16 vs or inv
                         vs();
                     }
