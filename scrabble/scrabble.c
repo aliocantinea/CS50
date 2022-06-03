6476e3141f8cf1471d177f952e858ccebdf48797
aliocantinea <aliocantinea@gmail.com>
2022-06-03 03:07:03 +0000

    Fri, Jun 3, 2022, 3:07 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,9 +19,17 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    if (score1 > score 2)
+    if (score1 > score2)
     {
-        printf()
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
     }
 }
 
@@ -41,5 +49,6 @@ int compute_score(string word)
             score += POINTS[word[i] - 'a'];
         }
     }
+    return score;
 
 }
