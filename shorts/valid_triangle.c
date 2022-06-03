092b0453e488db9506697a5ccf4edff859ff845c
aliocantinea <aliocantinea@gmail.com>
2022-06-03 04:46:32 +0000

    Fri, Jun 3, 2022, 4:46 AM +00:00

---
+++ shorts/valid_triangle.c
@@ -0,0 +1,35 @@
+make#include <cs50.h>
+#include <stdio.h>
+
+int valid_triangle(int a, int b, int c);
+
+int main(void)
+{
+    int first = get_int("First side: ");
+    int second = get_int("Second side: ");
+    int third = get_int("Third side: ");
+
+    int tf = valid_triangle(first, second, third);
+    if(tf = 0)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+
+}
+
+int valid_triangle(int a, int b, int c)
+{
+    if((a + b) > c)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
