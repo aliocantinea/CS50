e4c832ee04a19b6ce15f75c2d86d213c9fef46bb
aliocantinea <aliocantinea@gmail.com>
2022-06-04 04:19:25 +0000

    Sat, Jun 4, 2022, 4:19 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int count_letters(string text);
 
@@ -18,6 +19,6 @@ int count_letters(string text)
 
     for (int i = 0, l = strlen(text); i < l; i++)
     {
-
+        if (islower)
     }
 }
