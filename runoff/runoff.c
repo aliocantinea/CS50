78bf41eb9b28ca35e8de0ad41dca89e63579cf0e
aliocantinea <aliocantinea@gmail.com>
2022-06-23 04:00:39 +0000

    Thu, Jun 23, 2022, 4:00 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,7 +129,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; ++i)
     {
         if (strcasecmp(name, candidates[i].name) == 0)
         {
@@ -144,10 +144,10 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     //for each voter
-    for (int i = 0; i < voter_count; i++)
+    for (int i = 0; i < voter_count; ++i)
     {
         //for each rank
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; ++j)
         {
             int pref = preferences[i][j];
             //if the jth preferences for voter ith is not eliminated
@@ -164,7 +164,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; ++i)
     {
         if (candidates[i].votes > candidate_count / 2)
         {
@@ -179,7 +179,7 @@ bool print_winner(void)
 int find_min(void)
 {
     int lowest = candidate_count;
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; ++i)
     {
         if (!candidates[i].eliminated)
         {
@@ -195,7 +195,7 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; ++i)
     {
         if (!candidates[i].eliminated)
         {
@@ -211,7 +211,7 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    for (int i = 0; i <candidate_count; i++)
+    for (int i = 0; i <candidate_count; ++i)
     {
         if (candidates[i].votes == min)
         {
