00fdec07a449f3d0eb7cd2826ba8f527342bca7e
aliocantinea <aliocantinea@gmail.com>
2022-06-23 02:00:28 +0000

    Thu, Jun 23, 2022, 2:00 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -95,7 +95,7 @@ void print_winner(void)
     {
         if (candidates[i].votes == winner)
         {
-            printf("%s\n", candidate[i].name);
+            printf("%s\n", candidates[i].name);
         }
     }
     return;
