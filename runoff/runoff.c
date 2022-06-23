f6ce10a464c686a322cb8502c3a5ba2c18b0f074
aliocantinea <aliocantinea@gmail.com>
2022-06-23 02:38:00 +0000

    Thu, Jun 23, 2022, 2:38 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -148,6 +148,14 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
+    int lowest = candidate_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            
+        }
+    }
     // TODO
     return 0;
 }
