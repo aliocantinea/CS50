9eb13cf1cae47f6a90109ed82629186716831186
aliocantinea <aliocantinea@gmail.com>
2022-06-23 03:15:35 +0000

    Thu, Jun 23, 2022, 3:15 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,7 +147,7 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (strcasecmp(preferences[i][j]))
+            if ()
         }
     }
     return;
