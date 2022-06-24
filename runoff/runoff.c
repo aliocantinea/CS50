ad15b344a485cc8a7e6422a89e57263f89922b18
aliocantinea <aliocantinea@gmail.com>
2022-06-24 05:13:39 +0000

    Fri, Jun 24, 2022, 5:13 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -165,7 +165,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int majority = candidate_count / 2;
+    int majority = voter_count / 2;
     for (int i = 0; i < candidate_count; ++i)
     {
         if (candidates[i].votes > majority)
@@ -183,7 +183,7 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int lowest = candidate_count;
+    int lowest = voter_count;
     for (int i = 0; i < candidate_count; ++i)
     {
         if (!candidates[i].eliminated)
