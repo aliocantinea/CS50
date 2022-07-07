1e852965741832fe561fc1393234ccc4522f5908
aliocantinea <aliocantinea@gmail.com>
2022-07-07 05:32:48 +0000

    Thu, Jul 7, 2022, 5:32 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -21,6 +21,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            int temp = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3.0;
+            if (temp > 255)
+            {
+                temp = 255;
+            }
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = temp;
+        }
+    }
     return;
 }
 
