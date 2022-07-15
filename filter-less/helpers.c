44526583a420d5409cec4164c165f110a3e90df4
aliocantinea <aliocantinea@gmail.com>
2022-07-15 05:27:51 +0000

    Fri, Jul 15, 2022, 5:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,9 +5,9 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             // adds together all 3 values and averages them
             // not sure why it has to be 3.0 vs 3
@@ -26,9 +26,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             // cal change to var and round to nearest int
             int SB = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
@@ -59,9 +59,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0, k = width - 1; j < width / 2; j++, k--)
+        for (int j = 0, k = width - 1; j < width / 2; j++, k--)
         {
             RGBTRIPLE tmp = image[i][j];
             image[i][j] = image[i][k];
@@ -80,16 +80,16 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     //variable for blur, so it could be incorporated later to be dynamic
     int blur = 1;
     // copy input into duplicate array
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             imgcpy[i][j] = image[i][j];
         }
     }
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             //set RGB struct to 0 each time you move to a pixel
             int SB = 0, SG = 0, SR = 0;
@@ -98,11 +98,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             float smpsize = 0;
             //take pixel from above and compare it to +1, =, -1 height and +1, =, -1 width
             //'1' here is equal to blur value of var at start of function
-            for(int k = (0 - blur); k < (0 + blur) + 1; k++)
+            for (int k = (0 - blur); k < (0 + blur) + 1; k++)
             {
-                for(int l = (0 - blur); l < (0 + blur) + 1; l++)
+                for (int l = (0 - blur); l < (0 + blur) + 1; l++)
                 {
-                    if(i + k < 0 || i + k >= height || j + l < 0 || j + l >= width)
+                    if (i + k < 0 || i + k >= height || j + l < 0 || j + l >= width)
                     {
                         //if outside of picture ignores pixel comparison by contining loop
                         continue;
@@ -116,9 +116,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
             //copy average from temp var to image
-            image[i][j].rgbtBlue = round(SB/smpsize);
-            image[i][j].rgbtGreen = round(SG/smpsize);
-            image[i][j].rgbtRed = round(SR/smpsize);
+            image[i][j].rgbtBlue = round(SB / smpsize);
+            image[i][j].rgbtGreen = round(SG / smpsize);
+            image[i][j].rgbtRed = round(SR / smpsize);
             //loops back to next pixel
         }
     }
