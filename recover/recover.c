68120ffe54b3bd20c7d00c05cc8546dd4933e947
aliocantinea <aliocantinea@gmail.com>
2022-07-30 21:53:27 +0000

    Sat, Jul 30, 2022, 9:53 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,12 +4,17 @@
 int main(int argc, char *argv[])
 {
     //check for argc < 2
+    if (argc > 1)
+    {
+        
+    }
 
     //check to see file can be opened
-    FILE *file = fopen(argv[1], "r");
+    FILE *file = fopen (argv[1], "r");
     if (file == NULL)
     {
-        printf()
+        printf ("Could not open %s.\n", argv[1])
+        return 1;
     }
 
     //read fole looking for jpeg header '0xff 0xd8 oxff 0xe...'
