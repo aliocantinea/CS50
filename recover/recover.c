c936e2392ee7d15830ff32baf43d10d7e7ed1006
aliocantinea <aliocantinea@gmail.com>
2022-08-01 04:40:56 +0000

    Mon, Aug 1, 2022, 4:40 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,13 +23,13 @@ int main(int argc, char *argv[])
     int blocksize = 512;
 
     //create buffer
-    uint8_t *buffer = malloc(blocksize * sizeof(uint8_t));
-    if (buffer == NULL)
-    {
-        printf("Not enough memory for buffer.\n");
-        fclose(file);
-        return 2;
-    }
+    uint8_t buffer[blocksize]; // = malloc(blocksize * sizeof(uint8_t));
+    // if (buffer == NULL)
+    // {
+    //     printf("Not enough memory for buffer.\n");
+    //     fclose(file);
+    //     return 2;
+    // }
 
     //assigns pointer for filename and returns in NULL
     char *filename = malloc(8 * sizeof(char));
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
     //read file looking for jpg header '0xff 0xd8 oxff 0xe...'
     while (fread(buffer, sizeof(uint8_t), blocksize, file) != 0)
     {
-        fread(buffer, sizeof(uint8_t), blocksize, file);
+        //fread(buffer, sizeof(uint8_t), blocksize, file);
         //if jpg header found open file
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
         (buffer[3] & 0xf0) == 0xe0)
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
             if (recovered != NULL)
             {
                     printf("Not enough memory for recovered file.\n");
-                    free(buffer);
+                    // free(buffer);
                     free(filename);
                     fclose(recovered);
                     fclose(file);
@@ -77,7 +77,7 @@ int main(int argc, char *argv[])
     }
 
     //free pointers
-    free(buffer);
+    // free(buffer);
     free(filename);
     //close files
     fclose(recovered);
