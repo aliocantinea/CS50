3fa62d2fa4874dc5e6ac657eeec8f344d0292c06
aliocantinea <aliocantinea@gmail.com>
2022-08-24 20:31:05 +0000

    Wed, Aug 24, 2022, 8:31 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -50,8 +50,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = *parent0;
-        p->parents[1] = *parent1;
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         p->alleles[0] = p->parents[0]->alleles[rand() % 2];
@@ -71,7 +71,7 @@ person *create_family(int generations)
     }
 
     // TODO: Return newly created person
-    return *p;
+    return p;
 }
 
 // Free `p` and all ancestors of `p`.
