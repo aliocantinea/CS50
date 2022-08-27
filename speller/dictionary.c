6502a495d5b201de7a4a975eef2d1292044146a5
aliocantinea <aliocantinea@gmail.com>
2022-08-27 21:12:33 +0000

    Sat, Aug 27, 2022, 9:12 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -51,7 +51,7 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     //open read file
-    FILE dict = fopen(dictionary, "r");
+    FILE *dict = fopen(dictionary, "r");
     if (NULL)
     {
         fclose(dictionary);
