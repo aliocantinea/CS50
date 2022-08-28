557d46c36cc4519db721c23abbabe09eb7529f97
aliocantinea <aliocantinea@gmail.com>
2022-08-28 00:41:02 +0000

    Sun, Aug 28, 2022, 12:41 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -150,7 +150,7 @@ bool unload(void)
         while (table[i]->next != NULL)
         {
             node *temp = table[i]->next;
-            table[i]->next = temp->next;
+            table[i] = temp->next;
             free(temp);
         }
 
