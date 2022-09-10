cdcffdda78cd623c30e8788cf186583fd263639f
aliocantinea <aliocantinea@gmail.com>
2022-09-10 23:19:19 +0000

    Sat, Sep 10, 2022, 11:19 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -31,9 +31,10 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range (N):
         winning_team = simulate_tournament(teams)
-        if winning_team in counts
+        if winning_team in counts:
             counts[winning_team] += 1
-        else
+        else:
+            counts.append(winning_team)
 
 
 
