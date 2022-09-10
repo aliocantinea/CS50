36d4b05423d6e44c1e30aa2556b167ec66419bc2
aliocantinea <aliocantinea@gmail.com>
2022-09-10 23:04:23 +0000

    Sat, Sep 10, 2022, 11:04 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -60,9 +60,11 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    winner = []
-
+    winners =[]
     
+    winners = simulate_round(teams)
+
+
     # Keep simulating until you have no more teams
     # This should work for any amount of even teams
     # A single winner should be returned
