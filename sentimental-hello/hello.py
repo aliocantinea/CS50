2b596bc2f0b4dabf54b0f19d341f17651577a895
aliocantinea <aliocantinea@gmail.com>
2022-09-11 01:08:09 +0000

    Sun, Sep 11, 2022, 1:08 AM +00:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -3,7 +3,10 @@
 def main ():
     try:
         name = input("What's your name? ")
-
+    except ValueError:
+        print("Is that your real name though?")
+        exit()
+    print("Hello, {name}!")
 
 if __name__ == "__main__":
     main()
