c09cd39b0415536d565862fdcc3a0e82729d6c9d
aliocantinea <aliocantinea@gmail.com>
2022-09-19 00:17:13 +0000

    Mon, Sep 19, 2022, 12:17 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,7 +2,15 @@
 from cs50 import get_float
 
 
-# Get input
+# Get amount of change from user
+def GetChange()
+    try:
+        Cents = float(input("How much changed is owed: "))
+        # Make sure it is a possitive float
+        while Cents > 0
+    except ValueError:
+        print("")
+        Cents = GetChange()
+    return Cents
 # Make sure it is a positive float
-# 
 # Print minimum amount of coins to make change as an int
