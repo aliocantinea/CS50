3893f1bfeb63a9f83000f57ec2c028a8cbc8730a
aliocantinea <aliocantinea@gmail.com>
2022-09-19 02:20:03 +0000

    Mon, Sep 19, 2022, 2:20 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -14,8 +14,9 @@ coins = [
 # Get amount of change from user
 def get_change():
     try:
+        # Ask for float while input is less than 0
         change = float(input("How much changed is owed: "))
-        while float(change) <= 0:
+        while float(change) < 0:
             change = float(input("A positive floating point number please: "))
     except ValueError:
         # Recursively ask for float
@@ -23,16 +24,21 @@ def get_change():
     return change
 
 
-#
+# Call get_change for input and set total_change to 0 to start
 cents = get_change()
-print(cents)
 total_change = 0
+# For each coin key in tuple coins do the following
 for coin, value in coins:
+    # How many total times coin goes into change without remainder
     counter = int(cents / float(value))
-    print(counter)
-    cents = round(cents - (counter * float(value)))
-    print(cents)
+    # To deal with floating point number imprecision use round(#, ndigits)
+    # https://docs.python.org/3/library/functions.html#round
+    cents = round(cents - (counter * float(value)), 2)
+    # Add to how many coins are need before going to next key in tuple
     total_change += counter
+
+
+# Return int
 print(total_change)
 
 
