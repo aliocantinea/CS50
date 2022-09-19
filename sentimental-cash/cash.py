72ac0297ecbea09e2b66d6549e73eef1459d295d
aliocantinea <aliocantinea@gmail.com>
2022-09-19 00:27:42 +0000

    Mon, Sep 19, 2022, 12:27 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,13 +6,19 @@ from cs50 import get_float
 def GetChange()
     try:
         Change = float(input("How much changed is owed: "))
-        # Make sure it is a possitive float
+        # Make sure it is a positive float
         while Change > 0
     except ValueError:
+        # Recursively calls function if not a float
         print("Please input change formated like eg: 9 or 9.75")
         Change = GetChange()
     return Change
 
+Cents = GetChange()
+# Calculates amount of quaters
+Quaters = Cents / 25
+Coins = Cents - Quaters * 25
+
 
 # Calculates amount of quaters
 
@@ -26,8 +32,3 @@ def GetChange()
 
 
 
-# Calculates amount of quaters
-
-
-
-Change = GetChange()
