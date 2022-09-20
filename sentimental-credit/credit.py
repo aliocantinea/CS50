0af3c432d35a6c9acc69a3c2f0a06352dc70e8d5
aliocantinea <aliocantinea@gmail.com>
2022-09-20 03:04:29 +0000

    Tue, Sep 20, 2022, 3:04 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,13 +2,6 @@
 # https://docs.python.org/3/library/re.html
 
 # TODO
-cards = {
-    "inv": INVALID,
-    "vs": VISA,
-    "MASTERCARD": 16,
-    "AMEX": 15
-}
-
-
 def get_cardnumber()
     cardnum = int(input("Card number: "))
+    
