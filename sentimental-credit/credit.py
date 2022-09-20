df0a7a72bfd0dd4fafd15b6f6effce9eac4fe987
aliocantinea <aliocantinea@gmail.com>
2022-09-20 02:42:12 +0000

    Tue, Sep 20, 2022, 2:42 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,7 +2,14 @@
 # https://docs.python.org/3/library/re.html
 
 # TODO
+class CardTypes():
 
+    def __init__(self, card, type):
+        self.card = card
+        self.type = type
+
+    def print(self):
+        print(f"{self.type}")
 
 def get_cardnumber()
     cardnum = int(input("Card number: "))
