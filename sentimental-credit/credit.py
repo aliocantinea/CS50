6884811c8b1293e00a0ea7b930454d72e3f6660d
aliocantinea <aliocantinea@gmail.com>
2022-09-20 02:56:21 +0000

    Tue, Sep 20, 2022, 2:56 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,22 +2,13 @@
 # https://docs.python.org/3/library/re.html
 
 # TODO
-class CardType():
-
-    def __init__(self, card, type):
-        self.card = card
-        self.type = type
-
-    def print(self):
-        print(f"{self.type}")
-
-
-inv = CardType("Invalid", INVALID)
-visa = CardType("Visa", VISA)
-mc = CardType("Mastercard", MASTERCARD)
-am = CardType("American_Express", AMEX)
+cards = {
+    "inv": INVALID,
+    "vs": VISA,
+    "mc": MASTERCARD,
+    "am": AMEX
+}
 
 
 def get_cardnumber()
     cardnum = int(input("Card number: "))
-    
