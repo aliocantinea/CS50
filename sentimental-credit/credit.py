eec953da76ad970e2278fa7e8f939ecb32267027
aliocantinea <aliocantinea@gmail.com>
2022-09-20 03:08:09 +0000

    Tue, Sep 20, 2022, 3:08 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,7 +2,11 @@
 # https://docs.python.org/3/library/re.html
 
 # TODO
+# Global constants
+maxcard = 16
+mincard = 13
+
 def get_cardnumber()
     cardnum = int(input("Card number: "))
     while True:
-        
+        if(len(cardnum) <
