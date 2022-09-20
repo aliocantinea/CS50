7612f9046abe58f4957d76eeac6d168a88cfff99
aliocantinea <aliocantinea@gmail.com>
2022-09-20 03:13:01 +0000

    Tue, Sep 20, 2022, 3:13 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,12 +1,18 @@
 # Referenced docs.python.org as suggested
 # https://docs.python.org/3/library/re.html
 
-# TODO
-# Global constants
-maxcard = 16
-mincard = 13
 
+# TODO
+
+
+# Global constants outside of CS50 create a constant.py and place in var
+# Include import constant and reference them as constant.MAXCARD or constant.MINCARD
+MAXCARD = 16
+MINCARD = 13
+
+
+# Get user input for card number
 def get_cardnumber()
     cardnum = int(input("Card number: "))
     while True:
-        if(len(cardnum) <
+        if(len(cardnum) < MAXCARD 
