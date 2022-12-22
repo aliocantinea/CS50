4d4418b3e96d54584485ff342876dd2b419b6097
aliocantinea <aliocantinea@gmail.com>
2022-12-22 01:46:14 +0000

    Thu, Dec 22, 2022, 1:46 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -118,6 +118,9 @@ def quote():
 def register():
     """Register user"""
 
+    # Forget any user_id
+    session.clear()
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
