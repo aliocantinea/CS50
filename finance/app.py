b14fb50e9f4a8e79c9150f5325712fbc68dd8e45
aliocantinea <aliocantinea@gmail.com>
2022-12-22 02:25:24 +0000

    Thu, Dec 22, 2022, 2:25 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -147,8 +147,9 @@ def register():
         if len(rows) > 0:
             return render_template("login.html")
 
+        password = generate_password_hash(request.form.get("password"),method='pbkdf2:sha256', salt_length=8)
         # Register user
-        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (request.form.get("username"), generate_password_hash(request.form.get("password"),method='pbkdf2:sha256', salt_length=8)))
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (request.form.get("username"), password))
 
         # Remember which user has registered
         session["user_id"] = rows[0]["id"]
