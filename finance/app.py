370b3c981767b26a46fd9268cafd4e19afed8037
aliocantinea <aliocantinea@gmail.com>
2022-12-22 03:21:14 +0000

    Thu, Dec 22, 2022, 3:21 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -111,7 +111,10 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
