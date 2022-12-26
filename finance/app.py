7107a982241f92708122c2c6b6bb9a66c2c0e57b
aliocantinea <aliocantinea@gmail.com>
2022-12-26 23:22:35 +0000

    Mon, Dec 26, 2022, 11:22 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -70,7 +70,7 @@ def buy():
         # Check sufficient funds to buy
         cost = (query["price"] * shares)
         cash = db.execute("SELECT cash FROM users WHERE username = ?", session["user_id"])
-        if (int(cash) - cost) < 0:
+        if (int(cash["cash"]) - cost) < 0:
             return apology("Insufficient funds", 507)
         else:
            wallet  = cash[1] - cost
